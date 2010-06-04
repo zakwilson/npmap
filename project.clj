d53a6612392ac9feca99fd53fb0027457f96622f
Zak <zak@blackbox.(none)>
2010-06-04 17:17:08 -0400

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject npmap "1.0.0-SNAPSHOT"
+  :description "Parallelism test"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :main npmap.core)
---
+++ test/npmap/core_test.clj
@@ -0,0 +1,6 @@
+(ns npmap.core-test
+  (:use [npmap.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
